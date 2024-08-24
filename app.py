557faf074249ad39c7ea784bcdae7508d63a8304
my name <shailesh.suthar@saarthee.com>
2024-08-24 13:48:46 +0530

    Added dynamic data and api cloud.

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+jobs = [
+    {
+        "Job Title": "Data Analyst",
+        "Location": "Pune",
+        "Salary": "$100,000",
+    },
+    {
+        "Job Title": "Data Scientist",
+        "Location": "Gurgaon",
+        "Salary": "$120,000",
+    },
+    {
+        "Job Title": "Data Analyst",
+        "Location": "Bengalore",
+        "Salary": "$90,000",
+    },
+    {
+        "Job Title": "Software Development Engineer",
+        "Location": "Mumbai",
+        "Salary": "$950,000",
+    },
+]
 
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=jobs)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
